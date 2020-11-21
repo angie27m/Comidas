6efe8d77983f9c6b72c4999e50a9f1a60b36b755
angie27m <angie27manrique@gmail.com>
2020-11-21 00:19:06 -0500

    Tomcat

--- Comidas/src/main/java/com/udec/Spring1Application.java
+++ Comidas/src/main/java/com/udec/Spring1Application.java
@@ -2,12 +2,18 @@ package com.udec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Spring1Application {
+public class Spring1Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring1Application.class, args);
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Spring1Application.class);
+	}
 }
