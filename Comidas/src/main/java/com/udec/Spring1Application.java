3b5032961133791a6b01420d5ae3db62cee38aad
angie27m <angie27manrique@gmail.com>
2020-11-26 10:06:17 -0500

    Commit

--- Comidas/src/main/java/com/udec/Spring1Application.java
+++ Comidas/src/main/java/com/udec/Spring1Application.java
@@ -14,7 +14,7 @@ public class Spring1Application extends SpringBootServletInitializer{
 		SpringApplication.run(Spring1Application.class, args);
 	}
 	
-	//Cambio para commit nuevo
+	//Cambio para commit nuevo x2
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Spring1Application.class);
